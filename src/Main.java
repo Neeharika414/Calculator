b3cb9b4edcd56898746184ca1e3d97951d028f96
wasvi <wasvisahil09@gmail.com>
2023-10-19 12:15:35 +0530

    DEV push

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,56 @@
-
-
+import java.util.Scanner;
 
 class Main {
+  public static void main(String[] args) {
 
-	  public static void main(String[] args) {
-	     
-	    int first = 10;
-	    int second = 20;
+    char operator;
+    Double number1, number2, result;
 
-	    // add two numbers
-	    int sum = first + second;
-	    System.out.println(first + " + " + second + " = "  + sum);
-	  }
-	}
+    // create an object of Scanner class
+    Scanner input = new Scanner(System.in);
+
+    // ask users to enter operator
+    System.out.println("Choose an operator: +, -, *, or /");
+    operator = input.next().charAt(0);
+
+    // ask users to enter numbers
+    System.out.println("Enter first number");
+    number1 = input.nextDouble();
+
+    System.out.println("Enter second number");
+    number2 = input.nextDouble();
+
+    switch (operator) {
+
+      // performs addition between numbers
+      case '+':
+        result = number1 + number2;
+        System.out.println(number1 + " + " + number2 + " = " + result);
+        break;
+
+      // performs subtraction between numbers
+      case '-':
+        result = number1 - number2;
+        System.out.println(number1 + " - " + number2 + " = " + result);
+        break;
+
+      // performs multiplication between numbers
+      case '*':
+        result = number1 * number2;
+        System.out.println(number1 + " * " + number2 + " = " + result);
+        break;
+
+      // performs division between numbers
+      case '/':
+        result = number1 / number2;
+        System.out.println(number1 + " / " + number2 + " = " + result);
+        break;
+
+      default:
+        System.out.println("Invalid operator!");
+        break;
+    }
+
+    input.close();
+  }
+}
